ad6a48ebc0cb10e3a0c7110d90e99d147b547378
Anchit Kalra <53076375+AnchitKalra@users.noreply.github.com>
2021-02-27 01:18:55 +0530

    Update Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://3.89.199.35:8080/api/v1/";
+    this.baseUrl = "http://18.235.233.187:8080/api/v1/";
   }
   render(){
     return(
